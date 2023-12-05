e3a36ab228ffb8ea009679f4ad48dfe100003838
Ahmednas211 <nasiruahmed212@gmail.com>
2023-12-05 17:13:55 -0600

    updates changes

--- vpc module/main.tf
+++ vpc module/main.tf
@@ -79,7 +79,7 @@ resource "aws_subnet" "private_app_subnet_az1" {
   map_public_ip_on_launch = false
 
   tags = {
-    Name = "${var.project_name}-${var_environment}-private-app-az1"
+    Name = "${var.project_name}-${var.environment}-private-app-az1"
   }
 }
 
@@ -87,7 +87,7 @@ resource "aws_subnet" "private_app_subnet_az1" {
 resource "aws_subnet" "private_app_subnet_az2" {
   vpc_id                  = aws_vpc.vpc.id
   cidr_block              = var.private_app_subnet_az2_cidr
-  availability_zone       = data.aws_availability_zones.available_zones.anmes[1]
+  availability_zone       = data.aws_availability_zones.available_zones.names[1]
   map_public_ip_on_launch = false
 
   tags = {
@@ -111,7 +111,7 @@ resource "aws_subnet" "private_data_subnet_az1" {
 resource "aws_subnet" "private_data_subnet_az2" {
   vpc_id                  = aws_vpc.vpc.id
   cidr_block              = var.private_data_subnet_az2_cidr
-  availability_zone       = data.aws_availability_zones.available_zones.name[1]
+  availability_zone       = data.aws_availability_zones.available_zones.names[1]
   map_public_ip_on_launch = false
 
   tags = {
