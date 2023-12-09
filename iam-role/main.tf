8576fc3f746afb20760ee0e99b9729a21e3f87ce
Ahmednas211 <nasiruahmed212@gmail.com>
2023-12-09 16:38:27 -0600

    IAM Role Update

---
+++ iam-role/main.tf
@@ -0,0 +1,65 @@
+# create iam policy document. this policy allows the ecs service to assume a role
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+# create iam policy document
+data "aws_iam_policy_document" "ecs_task_execution_policy_document" {
+  statement {
+    actions = [
+      "ecr:GetAuthorizationToken",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:BatchGetImage",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents"
+    ]
+
+    resources = ["*"]
+  }
+
+  statement {
+    actions = [
+      "s3:GetObject"
+    ]
+
+    resources = [
+      "arn:aws:s3:::${var.project_name}-${var.env_file_bucket_name}/*"  # This is the ARN of the s3 bucket, because we want the ECS service to the get any object from this s3 bucket
+    ]
+  }
+
+  statement {
+    actions = [
+      "s3:GetBucketLocation"  # we want the ECS service the get the s3 location.
+    ]
+
+    resources = [
+      "arn:aws:s3:::${var.project_name}-${var.env_file_bucket_name}"  # Here, we want the ARN of the s3 bucket -- just like what we did in line 34
+    ]
+  }
+}
+
+# create iam policy
+resource "aws_iam_policy" "ecs_task_execution_policy" {
+  name   = "${var.project_name}-${var.environment}-ecs-task-execution-role-policy" # the variable here is our typical project name and environmnet information
+  policy = data.aws_iam_policy_document.ecs_task_execution_policy_document.json
+}
+
+# create an iam role
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name               = "${var.project_name}-${var.environment}-ecs-task-execution-role"
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+}
+
+# attach ecs task execution policy to the iam role
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role" {
+  role       = aws_iam_policy.ecs_task_execution_policy.name
+  policy_arn = aws_iam_policy.ecs_task_execution_policy.arn
+}
