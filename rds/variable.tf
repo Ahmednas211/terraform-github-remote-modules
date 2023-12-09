c904e50b94cf1906dd5558f2500697cfdfdad1a2
Ahmednas211 <nasiruahmed212@gmail.com>
2023-12-09 06:16:35 -0600

    rds update

---
+++ rds/variable.tf
@@ -0,0 +1,12 @@
+# rds varaiable
+variable "project_name" {}
+variable "environment" {}
+variable "private_data_subnet_az1_id" {}
+variable "private_data_subnet_az2_id" {}
+variable "db_snapshot_identifier" {}
+variable "db_snapshot_identifier" {}
+variable "db_instance_class" {}
+variable "availability_zone_1" {}
+variable "db_instance_identifier" {}
+variable "multi_az_depoyment" {}
+variable "database_security_group_id" {}
