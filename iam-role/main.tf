0cf8cf820ef8d20e5a1fab1f6f1dddf1fb117d29
Ahmed <nasiruahmed212@gmail.com>
2024-01-06 09:55:30 -0600

    updated iam role

--- iam-role/main.tf
+++ iam-role/main.tf
@@ -1,4 +1,4 @@
-# IAM Policy Document for assuming a role
+# create iam policy document. this policy allows the ecs service to assume a role
 data "aws_iam_policy_document" "assume_role_policy" {
   statement {
     actions = ["sts:AssumeRole"]
@@ -10,7 +10,7 @@ data "aws_iam_policy_document" "assume_role_policy" {
   }
 }
 
-# IAM Policy Document for ECS Task Execution
+# create iam policy document
 data "aws_iam_policy_document" "ecs_task_execution_policy_document" {
   statement {
     actions = [
@@ -19,37 +19,47 @@ data "aws_iam_policy_document" "ecs_task_execution_policy_document" {
       "ecr:GetDownloadUrlForLayer",
       "ecr:BatchGetImage",
       "logs:CreateLogStream",
-      "logs:PutLogEvents",
+      "logs:PutLogEvents"
     ]
+
     resources = ["*"]
   }
 
-  # create an inline policy to access s3 object and get bucket location
   statement {
-    actions = ["s3:GetObject"]
-    resources = ["arn:aws:s3:::${var.project_name}-${var.env_file_bucket_name}/*"]
+    actions = [
+      "s3:GetObject"
+    ]
+
+    resources = [
+      "arn:aws:s3:::${var.project_name}-${var.env_file_bucket_name}/*"
+    ]
   }
 
   statement {
-    actions = ["s3:GetBucketLocation"]
-    resources = ["arn:aws:s3:::${var.project_name}-${var.env_file_bucket_name}"]
+    actions = [
+      "s3:GetBucketLocation"
+    ]
+
+    resources = [
+      "arn:aws:s3:::${var.project_name}-${var.env_file_bucket_name}"
+    ]
   }
 }
 
-# IAM Policy for ECS Task Execution
+# create iam policy
 resource "aws_iam_policy" "ecs_task_execution_policy" {
-  name   = "${var.project_name}-${var.environment}-ecs-task-execution-policy"
+  name   = "${var.project_name}-${var.environment}-ecs-task-execution-role-policy"
   policy = data.aws_iam_policy_document.ecs_task_execution_policy_document.json
 }
 
-# create an IAM role
+# create an iam role
 resource "aws_iam_role" "ecs_task_execution_role" {
   name               = "${var.project_name}-${var.environment}-ecs-task-execution-role"
   assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
 }
 
-# Attach ECS Task Execution Policy to the IAM Role
+# attach ecs task execution policy to the iam role
 resource "aws_iam_role_policy_attachment" "ecs_task_execution_role" {
   role       = aws_iam_role.ecs_task_execution_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceRole"
+  policy_arn = aws_iam_policy.ecs_task_execution_policy.arn
 }
